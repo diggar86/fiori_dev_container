943aa1c56b31613d2d9f4cf8f688052d4dc1b2e9
fisherman <jandrees97@gmail.com>
2022-12-21 09:01:21 +0000

    first commit

---
+++ playground/webapp/test/integration/opaTests.qunit.js
@@ -0,0 +1,7 @@
+/* global QUnit */
+
+sap.ui.require(["org/fisherman/playground/test/integration/AllJourneys"
+], function () {
+	QUnit.config.autostart = false;
+	QUnit.start();
+});
---
+++ playground/webapp/test/unit/AllTests.js
@@ -0,0 +1,5 @@
+sap.ui.define([
+	"orgfisherman/playground/test/unit/controller/View1.controller"
+], function () {
+	"use strict";
+});
